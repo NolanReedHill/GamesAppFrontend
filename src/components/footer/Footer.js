d5a95c645a7ce682a4dbecef24a56d19fdc4231a
unknown <nolanreedhill@gmail.com>
2023-06-22 17:11:39 -0400

    changed footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,58 +13,20 @@ export default function Footer() {
             sx={{ marginTop: "10%", height: "fit-content", width: "100%", marginBottom: "0%", backgroundColor: "#036627" }}
         >
             <Container maxWidth="lg">
-                <Grid container spacing={5}>
+                <Grid container spacing={5} justifyContent="center">
                     <Grid item xs={12} sm={4}>
                         <Typography variant="h6" color="text.primary" gutterBottom>
-                            About Us
+                            About
                         </Typography>
                         <Typography variant="body2" color="text.secondary" textAlign="center">
-                            Welcome to EasyBuy, the shopping solution you've been looking for.
+                            My game website. Have fun!
                         </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Typography variant="h6" color="text.primary" gutterBottom>
-                            Contact Us
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" textAlign="center">
-                            123 Main Street, Anytown, USA
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" textAlign="center">
-                            Email: support.easyBuy@easyBuy.com
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" textAlign="center">
-                            Phone: +1 234 567 8901
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={4} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
-                        <Typography variant="h6" color="text.primary" gutterBottom>
-                            Follow Us
-                        </Typography>
-                        <div style={{ display: "flex", justifyContent: "center" }}>
-                            <Link href="https://www.facebook.com/" color="inherit">
-                                <Facebook />
-                            </Link>
-                            <Link
-                                href="https://www.instagram.com/"
-                                color="inherit"
-                                sx={{ pl: 1, pr: 1 }}
-                            >
-                                <Instagram />
-                            </Link>
-                            <Link href="https://www.twitter.com/" color="inherit">
-                                <Twitter />
-                            </Link>
-                        </div>
                     </Grid>
                 </Grid>
                 <Box mt={5}>
                     <Typography variant="body2" color="text.secondary" align="center" >
-                        {"Copyright © "}
-                        <Link color="inherit" href="https://glittery-malasada-22e17e.netlify.app/">
-                            EasyBuy
-                        </Link>{" "}
+                        {"Nolan Hill "}
                         {new Date().getFullYear()}
-                        {"."}
                     </Typography>
                 </Box>
             </Container>
